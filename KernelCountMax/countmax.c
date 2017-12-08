94833a87ff1b73cc21916386c6933be380c81b87
C.Venn <genius-littley@foxmail.com>
2017-12-08 17:52:56 +0800

    kernel

--- KernelCountMax/countmax.c
+++ KernelCountMax/countmax.c
@@ -1,8 +1,9 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include "countmax.h"
 #include "linux/slab.h"
 
-static int get_hash_code(struct flow_key* key) {
+static int flow_key_hash(struct flow_key* key) {
     int hashCode = (int)key->srcip;
     hashCode = (hashCode * 397) ^ (int)key->dstip;
     hashCode = (hashCode * 397) ^ (int)key->srcport;
@@ -21,11 +22,12 @@ struct countmax_line* new_countmax_line(int w) {
     line->counters = (elemtype*)kzalloc(w * sizeof(elemtype), GFP_ATOMIC);
     line->keys = (struct flow_key*)kzalloc(w * sizeof(struct flow_key), GFP_ATOMIC);
     line->w = w;
+    line->mask = random();
     return line;
 }
 
 void countmax_line_update(struct countmax_line* this, struct flow_key* key, elemtype value) {
-    size_t index = get_hash_code(key) % this->w;
+    size_t index = (uint32_t)flow_key_hash(key) % this->w;
     struct flow_key* current_key = &(this->keys[index]);
     if (flow_key_equal(key, current_key)) {
         this->counters[index] += value;
@@ -43,7 +45,7 @@ void countmax_line_update(struct countmax_line* this, struct flow_key* key, elem
 }
 
 elemtype countmax_line_query(struct countmax_line* this, struct flow_key* key) {
-    size_t index = get_hash_code(key) % this->w;
+    size_t index = (uint32_t)flow_key_hash(key) % this->w;
     struct flow_key* current_key = &(this->keys[index]);
     if (flow_key_equal(key, current_key)) {
         return this->counters[index];
@@ -79,4 +81,34 @@ elemtype countmax_sketch_query(struct countmax_sketch* this, struct flow_key* ke
         }
     }
     return max;
+}
+
+void countmax_manager_update(struct countmax_manager* this, int sw_id, struct flow_key* key, elemtype value) {
+    if (sw_id < 0 || sw_id >= this->sw_count) {
+        return;
+    }
+    countmax_sketch_update(this->sketches[sw_id], key, value);
+}
+
+elemtype countmax_manager_query(struct countmax_manager* this, struct flow_key* key) {
+    elemtype max = 0;
+    for (int i = 0; i < this->sw_count; i++) {
+        elemtype q = countmax_sketch_query(this->sketches[i], key);
+        if (q > max) {
+            max = q;
+        }
+    }
+    return max;
+}
+
+struct countmax_manager* new_countmax_manager(int w, int d, int sw_count) {
+    struct countmax_manager* manager = kzalloc(sizeof(struct countmax_manager), GFP_ATOMIC);
+    manager->w = w;
+    manager->d = d;
+    manager->sw_count = sw_count;
+    manager->sketches = kzalloc(sw_count * sizeof(struct countmax_sketch*), GFP_ATOMIC);
+    for (int i = 0; i < sw_count; i++) {
+        manager->sketches[i] = new_countmax_sketch(w, d);
+    }
+    return manager;
 }
--- KernelCountMax/countmax.h
+++ KernelCountMax/countmax.h
@@ -1,10 +1,7 @@
 #ifndef COUNTMAX_H
 #define COUNTMAX_H
 #include <stdint.h>
-#include <ctype.h>
-#include <locale.h>
 #include <wchar.h>
-#include <stddef.h>
 typedef int64_t elemtype;
 struct flow_key {
     uint32_t srcip;
@@ -13,11 +10,12 @@ struct flow_key {
     uint16_t dstport;
     uint16_t protocol;
 };
-static int get_hash_code(struct flow_key* key);
+static int flow_key_hash(struct flow_key* key);
 static int flow_key_equal(struct flow_key* lhs, struct flow_key* rhs);
 
 struct countmax_line {
     size_t w;
+    uint32_t mask;
     struct flow_key* keys;
     elemtype* counters;
 };
@@ -34,4 +32,14 @@ struct countmax_sketch {
 static struct countmax_sketch* new_countmax_sketch(int w, int d);
 static void countmax_sketch_update(struct countmax_sketch* this, struct flow_key* key, elemtype value);
 static elemtype countmax_sketch_query(struct countmax_sketch* this, struct flow_key* key);
+
+struct countmax_manager {
+    size_t w;
+    size_t d;
+    size_t sw_count;
+    struct countmax_sketch** sketches;
+};
+static struct countmax_manager* new_countmax_manager(int w, int d, int sw_count);
+static void countmax_manager_update(struct countmax_manager* this, int sw_id, struct flow_key* key, elemtype value);
+static elemtype countmax_manager_query(struct countmax_manager* this, struct flow_key* key);
 #endif
